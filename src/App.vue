bbb9c014849a1e0584d67f4cc2b167e96b8cd106
Stephan Langeveld <stephanlangeveld@gmail.com>
2020-08-24 12:46:22 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+<p class="bg-blue-500 bg-opacity-100 text-6xl uppercase">hello world</p>
+ <fly to="full">
+   teleport full screen
+ </fly>
+</template>
+
+<script>
+import fly from './components/fly.vue'
+
+export default {
+  name: 'App',
+  components: {
+    fly
+  }
+}
+</script>
---
+++ src/components/fly.vue
@@ -0,0 +1,60 @@
+<template>
+  <button @click="open = true">open telporpted modal</button>
+
+  <br />
+
+  <teleport to="#portal">
+    <div v-if="open" :style="portal[to]">
+      <div class="modal bg-blue-500">
+        <slot></slot>
+
+        <div class="absolute bottom-0">
+          <button @click="open = false">close</button>
+        </div>
+      </div>
+    </div>
+  </teleport>
+</template>
+
+
+<script>
+export default {
+  props: ["to"],
+  name: "fly",
+  props: {
+      to: {
+          default: "full",
+          validator: () => ["full", "left"]
+      }
+  },
+  data() {
+    return {
+      open: false,
+      portal: {
+        left: {
+          position: "fixed",
+          top: 0,
+          zIndex: 10,
+          backgroundColor: "#f5f5f5",
+          bottom: 0,
+          right: "75vw",
+          left: 0,
+        },
+
+        full: {
+          position: "fixed",
+          top: 0,
+          zIndex: 10,
+          // backgroundColor: "blue",
+          bottom: 0,
+          right: 0,
+          left: 0,
+        },
+      },
+    };
+  },
+};
+</script>
+
+<style>
+</style>
